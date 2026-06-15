bd42d0787fd8bc3765e6328570cc8017c9e58cd4
ranjeetk77080-commits <ranjeetk77080@gmail.com>
2026-06-15 20:52:00 +0530

    save

--- lib/app/core/config/api_config.dart
+++ lib/app/core/config/api_config.dart
@@ -1,4 +1,25 @@
+import 'package:flutter/foundation.dart';
+import 'dart:io';
+
+// https://difwa-backend.onrender.com/api
+final LiveUrl = 'https://difwa-backend.up.railway.app/api';
+
+final LocalUrl = Platform.isAndroid
+    ? 'http://10.0.2.2:5001/api'
+    : 'http://127.0.0.1:5001/api';
+
 class ApiConfig {
-  static const String baseUrl = 'https://difwa-backend.up.railway.app/api';
-  static const String socketUrl = 'https://difwa-backend.up.railway.app';
+  static String get baseUrl {
+    if (kDebugMode) {
+      return LocalUrl;
+    }
+    return LiveUrl;
+  }
+
+  static String get socketUrl {
+    if (kDebugMode) {
+      return LocalUrl;
+    }
+    return LiveUrl;
+  }
 }
